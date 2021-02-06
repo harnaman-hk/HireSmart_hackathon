36976feb49d427dd64f386340a80fe2f7705b704
harnaman-hk <harnaman.hk@gmail.com>
2021-02-06 19:44:04 +0530

    basic flask app

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result")
+def result():
+    return render_template("out.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
